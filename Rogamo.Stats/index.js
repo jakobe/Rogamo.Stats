b4bab6a6b52ef1acd77df43d872cc16aad94d367
Jakob Engelbrecht Olesen <jakob@basher.dk>
2016-03-03 09:19:00 +0100

    Added js-files to nsjproject file

--- Rogamo.Stats/index.js
+++ Rogamo.Stats/index.js
@@ -1,11 +1,14 @@
 (function () {
     'use strict';
-  var server = require("./server");
-  var router = require("./router");
-  var requestHandlers = require("./requestHandlers");
-  var handle = {};
-  handle["/"] = requestHandlers.start;
-  handle["/start"] = requestHandlers.start;
-  handle["/upload"] = requestHandlers.upload;
-  server.start(router.route, handle);
+    
+    var server = require("./server");
+    var router = require("./router");
+    var requestHandlers = require("./requestHandlers");
+    
+    var handle = {};
+    handle["/"] = requestHandlers.start;
+    handle["/start"] = requestHandlers.start;
+    handle["/upload"] = requestHandlers.upload;
+    
+    server.start(router.route, handle);
 })();
--- Rogamo.Stats/requestHandlers.js
+++ Rogamo.Stats/requestHandlers.js
@@ -1,25 +1,25 @@
 (function () {
     'use strict';
-  
-function start(response, postData) {
- console.log("Request handler 'start' was called.");
- response.writeHead(200, {"Content-Type": "text/plain"});
- response.write("Hello World from start");
- response.end();
- }
-
- function upload(response, postData) {
- console.log("Request handler 'upload' was called.");
-console.log("postData: " + postData);
-response.writeHead(200, {
-	"Content-Type": "text/plain",
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"
-});
- response.write("You've sent: " + postData);
- response.end();
- }
-
- exports.start = start;
- exports.upload = upload;
+    
+    function start(response, postData) {
+        console.log("Request handler 'start' was called.");
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Hello World from start");
+        response.end();
+    }
+    
+    function upload(response, postData) {
+        console.log("Request handler 'upload' was called.");
+        console.log("postData: " + postData);
+        response.writeHead(200, {
+            "Content-Type": "text/plain",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"
+        });
+        response.write("You've sent: " + postData);
+        response.end();
+    }
+    
+    exports.start = start;
+    exports.upload = upload;
 })();
