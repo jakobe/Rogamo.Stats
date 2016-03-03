646295ea3ffc95b2e0e5ed9386cef5f0e90a1b03
Jakob Engelbrecht Olesen <jakob@basher.dk>
2016-03-03 09:12:38 +0100

    Added app infrastructure

---
+++ Rogamo.Stats/index.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+  var server = require("./server");
+  var router = require("./router");
+  var requestHandlers = require("./requestHandlers");
+  var handle = {};
+  handle["/"] = requestHandlers.start;
+  handle["/start"] = requestHandlers.start;
+  handle["/upload"] = requestHandlers.upload;
+  server.start(router.route, handle);
+})();
--- Rogamo.Stats/server.js
+++ Rogamo.Stats/server.js
@@ -1,6 +1,34 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//(function () {
+    'use strict';
+
+ var http = require("http");
+ var url = require("url");
+
+ function start(route, handle) {
+ function onRequest(request, response) {
+ var postData = "";
+ var pathname = url.parse(request.url).pathname;
+ console.log("Request for " + pathname + " received.");
+
+ request.setEncoding("utf8");
+
+ request.addListener("data", function(postDataChunk) {
+ postData += postDataChunk;
+ console.log("Received POST data chunk '"+
+ postDataChunk + "'.");
+ });
+
+ request.addListener("end", function() {
+	console.log("POST data end");
+ route(handle, pathname, response, postData);
+ });
+
+
+ }
+
+ http.createServer(onRequest).listen(8888);
+ console.log("Server has started.");
+ }
+
+ exports.start = start;
+//})();
